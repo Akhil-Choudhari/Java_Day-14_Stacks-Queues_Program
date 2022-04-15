e5f78c88f5b8ca0a8c93debf47d1db2be5f2db55
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-04-15 14:36:58 +0530

    Stack and Queue program

---
+++ AkhilQueues.java
@@ -0,0 +1,14 @@
+package LinkedListProgram;
+
+public class AkhilQueues {
+
+LinkedList<T> myLinkedList;
+	
+	public AkhilQueues(){
+		myLinkedList = new LinkedList<T>();
+	}
+	
+	public void enqueue(T data){
+		myLinkedList.add(data);
+	}
+}
