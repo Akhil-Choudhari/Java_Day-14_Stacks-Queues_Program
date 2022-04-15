e79011c5eb7f883d436a027f7622b644c530e479
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-04-15 14:28:35 +0530

    Stacks and Queues java program

---
+++ AkhilStack.java
@@ -0,0 +1,23 @@
+package LinkedListProgram;
+
+public class AkhilStack<T> {
+MyLinkedList<T> myLinkedList;
+	
+	public MyStack(){
+		myLinkedList = new MyLinkedList<T>();
+	}
+	
+	public void push(T data){
+		myLinkedList.add(data);
+	}
+	
+	public T pop(){
+		return myLinkedList.pop();
+	}
+	
+	public T peek(){
+		T temp = myLinkedList.pop();
+		myLinkedList.add(temp);
+		return temp;
+	}
+
